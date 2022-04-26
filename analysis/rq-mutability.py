e0c45aece9214fa4d0e092a99a4eb671c78cf0bb
Samuel W. Flint <swflint@flintfam.org>
2022-04-26 13:10:26 -0500

    Drop old summarization code

--- analysis/rq-mutability.py
+++ analysis/rq-mutability.py
@@ -7,18 +7,6 @@ from matplotlib.ticker import PercentFormatter
 set_style()
 
 summarized = load_pre_summarized('kotlin', ['project', 'location', 'isval', 'isinferred'])
-
-# df = load_usage_data("kotlin")
-# total_counts = load_total_counts("kotlin")
-
-# counted = df.groupby(['project', 'location', 'isval', 'isinferred'], as_index = False).sum()[['project', 'location', 'isval', 'isinferred', 'count']]
-
-# summarized = counted.merge(total_counts, on=['project', 'location'], how = 'left')
-# summarized['percent'] = summarized.apply(lambda x: 0 if x['total'] == 0 else (x['count'] / x['total']) * 100, axis = 1)
-# summarized['isinferred'] = summarized.apply(lambda x: 'Inferred' if x.isinferred else 'Not Inferred', axis = 1)
-# summarized['isval'] = summarized.apply(lambda x: 'val' if x.isval else 'var', axis = 1)
-# summarized['location'] = summarized.apply(lambda x: {'return_val': "Return Value", 'body': "Body", 'module': "Top-Level Variables", 'lambda_arg': "Arguments List in Lambda", 'loop_variable': "Loop Variable"}[x.location], axis = 1)
-
 summarized = summarized[summarized['location'] != 'Return Value']
 
 plt.figure()
--- analysis/rq-usage.py
+++ analysis/rq-usage.py
@@ -8,16 +8,6 @@ set_style()
 
 summarized = load_pre_summarized('kotlin', ['project', 'location', 'isinferred'])
 
-# df = load_usage_data("kotlin")
-# total_counts = load_total_counts("kotlin")
-
-# counted = df.groupby(['project', 'location', 'isinferred'], as_index = False).sum()[['project', 'location', 'isinferred', 'count']]
-
-# summarized = counted.merge(total_counts, on=['project', 'location'], how='left')
-# summarized['percent'] = summarized.apply(lambda x: 0 if x['total'] == 0 else (x['count'] / x['total']) * 100, axis = 1)
-# summarized['isinferred'] = summarized['isinferred'].apply(inferred_name)
-# summarized['location'] = summarized['location'].apply(location_map)
-
 plt.figure()
 fig, ax = plt.subplots(1,1)
 sns.boxplot(x='location', y='percent', hue='isinferred', data=summarized, ax = ax, showfliers = False)
