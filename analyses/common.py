0aa875be0ced85e48563a81db1e9070a2fbb1362
Samuel W. Flint <swflint@flintfam.org>
2022-06-07 10:27:39 -0500

    Add original summarisation code back in

--- analyses/common.py
+++ analyses/common.py
@@ -131,3 +131,58 @@ def save_figure(figure, filename, x=None, y=None):
         fig.set(figheight = y)
     fig.savefig(filename, dpi=600)
     plt.close(fig)
+
+
+
+
+def load_data_csv(language, file, **kwargs):
+    return pd.read_csv(f"data/csv/{language}/{file}", on_bad_lines='skip', **kwargs)
+
+def load_usage_data(language):
+    path = f"data/parquet/usage-{language}.parquet"
+    if osp.exists(path):
+        return pd.read_parquet(path)
+    df = load_data_csv(language, "basic-usage.csv")
+    df.to_parquet(path)
+    return df
+
+def load_total_counts(language):
+    path = f"data/parquet/counts-{language}.parquet"
+    if osp.exists(path):
+        return pd.read_parquet(path)
+    df = load_usage_data(language)
+    df_counts = df.groupby(['project', 'location'], as_index = False).sum()[['project', 'location', 'count']].rename(columns = {'count': 'total'})[['project', 'location', 'total']]
+    df_counts.to_parquet(path)
+    return df_counts
+
+def load_pre_summarized(language, group_cols):
+    group_cols_string = '-'.join(group_cols)
+    path = f"data/parquet/counts-summarized-{language}-{group_cols_string}.parquet"
+    if osp.exists(path):
+        return pd.read_parquet(path)
+    df = load_usage_data(language)
+    df_totals = load_total_counts(language)
+    df_counted = df.groupby(group_cols, as_index = False).sum()[[*group_cols, 'count']]
+    df_summarized = df_counted.merge(df_totals, on=['project', 'location'], how = 'left')
+    df_summarized['percent'] = df_summarized.apply(lambda x: 0 if x['total'] == 0 else (x['count'] / x['total']) * 100, axis = 1)
+    if 'isinferred' in group_cols:
+        df_summarized['isinferred'] = df_summarized['isinferred'].apply(inferred_name)
+    if 'isval' in group_cols:
+        df_summarized['isval'] = df_summarized['isval'].apply(val_or_var)
+    if 'location' in group_cols:
+        df_summarized['location'] = df_summarized['location'].apply(location_map)
+    df_summarized.to_parquet(path)
+    return df_summarized
+
+def location_map(name):
+    return {'return_val': "Return Type",
+            'body': "Body",
+            'module': "Top Level",
+            'lambda_arg': "Lambda Args",
+            'loop_variable': "Loop Var"}[name]
+
+def inferred_name(isinferred):
+    return 'Inferred' if isinferred else 'Not Inferred'
+
+def val_or_var(isval):
+    return 'val' if isval else 'var'
