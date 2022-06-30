50cd0893e1a1c8e2eb9fc7ef1e398ad6775ea9c5
Samuel W. Flint <swflint@flintfam.org>
2022-06-30 10:36:13 -0500

    Update study configuration

--- analyses/rq-over-time.py
+++ analyses/rq-over-time.py
@@ -1,5 +1,29 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from common import *
+from common.local import *
+from common.tables import *
+from common.df import *
+import pandas as pd
 from matplotlib.ticker import PercentFormatter
+
+def get_time_data():
+    path_to_parquet = "data/parquet/kotlin/over-time-processed.parquet"
+    try:
+        df = pd.read_parquet(path_to_parquet)
+    except:
+        df = get_df("over-time", "kotlin", header='infer')
+        df['time'] = pd.to_datetime(df['time'], unit='us', origin='unix')
+        df.to_parquet(path_to_parquet, compression = 'gzip')
+    return df
+
+set_style()
+
+pd.set_option('display.max_columns', None)
+df = get_time_data()
+print(df.head())
+
+fig, ax = plt.subplots(1, 1)
+plt.figure()
+sns.lineplot(x='time', y='count', style='isinferred', hue='location', data=df, ax=ax, err_style='band', sort=True)
+save_figure(fig, "figures/rq-over-time-summary.pdf", 7, 4)
