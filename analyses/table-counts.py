1f1cf3817a53ea8bb6311817f745bc07599130cf
Samuel W. Flint <swflint@flintfam.org>
2022-07-05 18:01:16 -0500

    Prettify dataset stats table

--- analyses/table-counts.py
+++ analyses/table-counts.py
@@ -6,16 +6,28 @@ from common.tables import *
 from common.df import *
 
 df_unfiltered = get_df("count-unfiltered", "kotlin", header='infer')
-df_filtered = df_get("count-filtered", "kotlin", header='infer')
+df_filtered = get_df("count-filtered", "kotlin", header='infer')
 
 df = pd.concat([df_unfiltered, df_filtered])
+
 df['filtered'] = df['filtered'].apply(lambda x: "Post-Filtering" if x else "Pre-Filtering")
 df['type'] = df['type'].apply(lambda x: {'projects': "Projects",
                                          'total_files_head': "Total Files (HEAD)",
                                          'analyzed_files_head': "Analyzed Files (HEAD)",
                                          'total_files_hist': "Total Files (HIST)",
                                          'analyzed_files_hist': "Analyzed Files (HIST)"}[x])
+
 df_pivot = df.pivot(index = ['type'], columns = ['filtered'])
+
+df_pivot = df_pivot.reindex(['Projects',
+                             'Total Files (HEAD)',
+                             'Analyzed Files (HEAD)',
+                             'Total Files (HIST)',
+                             'Analyzed Files (HIST)'])
+
+df_pivot = df_pivot.sort_index(axis='columns', level='filtered', ascending=False)
+
+df_pivot = df_pivot.xs('count', axis='columns', drop_level=True)
 
 styler = highlight_rows(highlight_cols(get_styler(df_pivot)))
 save_table(styler, "dataset-counts.tex")
