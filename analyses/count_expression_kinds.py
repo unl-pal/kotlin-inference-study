1568bf9f019e4c0eeef6e27ba1af7e6b89d9dcd4
alikeshk <akeshk2@huskers.unl.edu>
2022-07-26 17:54:39 -0500

    updates to read csv, save table, clean dead code

--- analyses/count_expression_kinds.py
+++ analyses/count_expression_kinds.py
@@ -1,11 +1,13 @@
-#%%
+#!/usr/bin/env python
+# coding: utf-8
+
 import numpy as np
 import pandas as pd
 from common.tables import *
 from common.df import *
 pd.set_option('display.max_colwidth', None)
 
-df = pd.read_csv(r"right-hand-side-expression.csv", names=['id', 'file_path', 'isInferred', 'expressionKind', 'methodName', 'count'])
+df = get_df("right-hand-side-expression", "kotlin", names=['id', 'file_path', 'isInferred', 'expressionKind', 'methodName', 'count'])
 df = df[df.isInferred == "true"]
 
 expressionCategories = pd.DataFrame()
@@ -16,17 +18,16 @@ counts = []
 numLiterals = len(df[df.expressionKind == 'LITERAL'])
 counts.append(numLiterals)
 
-# numConstructors = len(df[(df.expressionKind == 'NEW') | (df['methodName'].astype(str).str[0].str.isupper())])
 numConstructors = len(df[df.methodName.astype(str).str[0].str.isupper()])
 counts.append(numConstructors)
 
 numNEW = len(df[df.expressionKind == 'NEW'])
 counts.append(numNEW)
 
-# numOther = len(df[(df.expressionKind != 'LITERAL') & (df.expressionKind != 'NEW') & (df.expressionKind != 'METHODCALL')])
 numOther = len(df) - numLiterals- numConstructors - numNEW
 counts.append(numOther)
 
 expressionCategories['Count'] = counts
 
-save_table(expressionCategories, "expression-categories.tex")
+styler = highlight_rows(highlight_cols(get_styler(expressionCategories)))
+save_table(styler, "expression-categories.tex")
