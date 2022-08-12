f69ed6232319126e87adb1eba211a8bd84f2ce68
alikeshk <akeshk2@huskers.unl.edu>
2022-08-12 12:30:39 -0500

    restart rq-rhs-types.py

--- analyses/rq-rhs-types.py
+++ analyses/rq-rhs-types.py
@@ -6,27 +6,4 @@ from common.tables import *
 from common.df import *
 pd.set_option('display.max_colwidth', None)
 
-df = get_df('right-hand-side-expression', 'kotlin', names=['id', 'file_path', 'isInferred', 'expressionKind', 'methodName', 'count'])
-df = df[df.isInferred == 'true']
-
-expressionCategories = pd.DataFrame()
-expressionCategories['Category'] = ['Literal', 'Constructor', 'NEW', 'Other']
-
-counts = []
-
-numLiterals = len(df[df.expressionKind == 'LITERAL'])
-counts.append(numLiterals)
-
-numConstructors = len(df[df.methodName.astype(str).str[0].str.isupper()])
-counts.append(numConstructors)
-
-numNEW = len(df[df.expressionKind == 'NEW'])
-counts.append(numNEW)
-
-numOther = len(df) - numLiterals- numConstructors - numNEW
-counts.append(numOther)
-
-expressionCategories['Count'] = counts
-
-styler = highlight_rows(highlight_cols(get_styler(expressionCategories)))
-save_table(styler, 'expression-categories.tex')
+df = get_df("determine-rhs-expression-types", "kotlin", header='infer')
