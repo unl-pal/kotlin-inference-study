0fa7daa17ccb85d6271ce805311f469cf3292e2f
alikeshk <akeshk2@huskers.unl.edu>
2022-08-17 13:17:09 -0500

    add 'project' to groupby, update describe data

--- analyses/rq-rhs-types.py
+++ analyses/rq-rhs-types.py
@@ -6,21 +6,20 @@ import pandas as pd
 from common.tables import *
 from common.df import *
 from common.local import *
-from common.tables import *
 from matplotlib.ticker import PercentFormatter
 from scipy.stats import shapiro
 pd.set_option('display.max_colwidth', None)
 
 df = get_df("determine-rhs-expression-types", "kotlin", header='infer')
-print(df['expkind'].describe())
 
 set_style()
-df_count = df[df.isinferred == True].drop(columns=['project','filepath','class','count'])
+df_count = df[df.isinferred == True].drop(columns=['filepath','class','count'])
 df_count['expkind'] = df_count.apply(lambda x: 'LITERAL' if x['expkind'] == 'LITERAL' else ('NEW' if x['expkind'] == 'NEW' else 'OTHER'), axis = 1)
-df_count = df_count.groupby(['isinferred', 'expkind'])['expkind'].count().reset_index(name='count')
+df_count = df_count.groupby(['project', 'isinferred', 'expkind'])['expkind'].count().reset_index(name='count')
 sum = df_count['count'].sum()
 df_count['percent'] = df_count.apply(lambda x: 0 if x['count'] == 0 else (x['count'] / sum) * 100, axis = 1)
 print(df_count)
+print(df_count['expkind'].describe())
 
 plt.figure()
 fig, ax = plt.subplots(1,1)
@@ -30,6 +29,7 @@ ax.set_ylabel("Percent per Project")
 ax.set_xlabel("")
 plt.xticks(rotation=90)
 plt.gca().legend().set_title("")
+
 # fig = figure.get_figure()
 plt.tight_layout()
 fig.set(figwidth = 7)
