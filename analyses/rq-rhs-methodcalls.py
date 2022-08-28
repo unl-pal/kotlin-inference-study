d7d911259e51346e5f8a5bcf90ca87ca203bc6eb
Robert Dyer <rdyer@unl.edu>
2022-08-27 20:09:47 -0500

    fix analysis

--- analyses/rq-rhs-methodcalls.py
+++ analyses/rq-rhs-methodcalls.py
@@ -11,11 +11,11 @@ from common.tables import get_styler, highlight_cols, highlight_rows, save_table
 pd.set_option('display.max_colwidth', None)
 
 df = get_deduped_df('method-calls-maybe-local', 'kotlin', header='infer')
-df = df[['maybelocal']].count()
-
+df2 = df[['project', 'maybelocal']].groupby('maybelocal').count()
+df2 = df2.rename(columns={'project': 'Number of Calls'})
 
 # %% generate the table
-styler = highlight_rows(highlight_cols(get_styler(df)))
+styler = highlight_rows(highlight_cols(get_styler(df2)))
 save_table(styler, 'rq-rhs-methodcalls.tex')
 
 # %%
