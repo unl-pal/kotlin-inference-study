ebdc7f019b8a6d7306c14d2bc5133611cf217e33
Robert Dyer <rdyer@unl.edu>
2022-08-28 02:38:30 -0500

    support running analyses from subdirs

--- analyses/common/utils.py
+++ analyses/common/utils.py
@@ -20,9 +20,10 @@ from typing import Optional
 
 def _resolve_dir(dir: str):
     curdir = os.getcwd()
-    if curdir.endswith('/analyses'):
-        return '../' + dir
-    return dir
+    parts = curdir.split('/analyses')
+    if len(parts) == 1:
+        return dir
+    return ''.join(['../' for x in range(len(parts))]) + dir
 
 
 def _get_dir(subdir: Optional[str]):
