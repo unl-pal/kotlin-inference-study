ebc445889fe826e119ea860ccd25b7a52a67b2a6
Samuel W. Flint <swflint@flintfam.org>
2022-08-28 17:12:45 -0500

    Add in a new usage graph generator

---
+++ analyses/kotlin/rq-usage-2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# %% build the dataframe
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+from common.graphs import setup_plots, save_figure
+from common.local import *
+from common.tables import *
+import matplotlib.ticker as mtick
+
+summarized = load_pre_summarized('kotlin',
+                                 ['project', 'location', 'isinferred'])
+
+summarized_sum = summarized.groupby(['project'])[['count']].sum().rename(columns={'count': 'total'})
+summarized = summarized.drop(columns={'total'}).merge(summarized_sum, on=['project'], how='left')
+summarized['percent'] = summarized.apply(
+    lambda x: 0 if x['total'] == 0 else (x['count'] / x['total']) * 100,
+    axis=1)
+
+# %% generate the plot
+fig, ax = setup_plots()
+sns.boxplot(x='location',
+            y='percent',
+            hue='isinferred',
+            data=summarized,
+            ax=ax,
+            showfliers=False)
+ax.set_ylabel('Percent per Project')
+ax.set_xlabel('')
+save_figure(fig, 'rq-usage-summary-2.pdf', subdir='kotlin')
+fig
+
+# %% generate the table
+data = summarized[['location', 'isinferred', 'percent']].groupby(['location', 'isinferred']).describe()
+styler = highlight_cols(highlight_rows(get_styler(data)))
+
+save_table(styler, 'rq-usage-summary-2.tex', subdir='kotlin')
