e47aea5273a5d1e8558f4411b138da2c0e9d3546
Samuel W. Flint <swflint@flintfam.org>
2023-01-13 13:21:53 -0600

    Add star count & devs to project summary

--- analyses/project-size.py
+++ analyses/project-size.py
@@ -11,9 +11,19 @@ import seaborn as sns
 
 df_java = get_df('project-size', 'java', header='infer')
 df_java['language'] = 'Java'
+df_java_stars = get_df('stars', 'java', header='infer').rename(columns={'stars': 'count'})
+df_java_stars['count_type'] = 'stars'
+df_java_devs = get_df('developer-count', 'java', header='infer').rename(columns={'developers': 'count'})
+df_java_devs['count_type'] = 'devs'
+df_java = pd.concat([df_java, df_java_stars, df_java_devs])
 
 df_kotlin = get_df('project-size', 'kotlin', header='infer')
 df_kotlin['language'] = 'Kotlin'
+df_kotlin_stars = get_df('stars', 'kotlin', header='infer').rename(columns={'stars': 'count'})
+df_kotlin_stars['count_type'] = 'stars'
+df_kotlin_devs = get_df('developer-count', 'kotlin', header='infer').rename(columns={'developers': 'count'})
+df_kotlin_devs['count_type'] = 'devs'
+df_kotlin = pd.concat([df_kotlin, df_kotlin_stars, df_kotlin_devs])
 
 df = pd.concat([df_java, df_kotlin]).reset_index()
 df_counts = df.loc[df.count_type != 'project_age'].copy(deep=True)
@@ -54,7 +64,9 @@ save_figure(fig, 'project-commits-summary.pdf', y=1.4)
 
 df_counts.count_type = df_counts.count_type.apply(lambda x: {'analyzed_commits': "Analyzed Commits",
                                                              'files': "Number of Files",
-                                                             'statements': "Number of Statements"}[x])
+                                                             'statements': "Number of Statements",
+                                                             'stars': "Number of Stars",
+                                                             'devs': "Number of Developers"}[x])
 df_summarized = df_counts.groupby(['language', 'count_type'])[['count']].describe()
 df_summarized = drop_outer_column_index(df_summarized).drop(columns=['count'])
 summarized_styler = highlight_cols(highlight_rows(get_styler(df_summarized)))
