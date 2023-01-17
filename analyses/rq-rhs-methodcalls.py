dca922c7137cba2bd0f0c47db51b7cf45cb70edf
Samuel W. Flint <swflint@flintfam.org>
2023-01-17 09:43:10 -0600

    Make autogeneration of table closer to desired

--- analyses/rq-rhs-methodcalls.py
+++ analyses/rq-rhs-methodcalls.py
@@ -11,20 +11,19 @@ df_kotlin = df_kotlin[['project', 'maybelocal']].groupby(['maybelocal']).count()
                 .rename(index={False: 'Not Local', True: 'Possibly File-Local'})
 kotlin_total_calls = df_kotlin.project.sum()
 df_kotlin['percent'] = df_kotlin.project.div(kotlin_total_calls).mul(100)
-df_kotlin['formatted'] = df_kotlin.apply(lambda x: "{:,.0f} ({:,.2f}%)".format(x.project, x.percent), axis=1)
-print(df_kotlin.formatted)
 
 df_java = get_deduped_df('method-calls-maybe-local', 'java', header='infer')
 df_java = df_java[['project', 'maybelocal']].groupby(['maybelocal']).count() \
                 .rename(index={False: 'Not Local', True: 'Possibly File-Local'})
 java_total_calls = df_java.project.sum()
 df_java['percent'] = df_java.project.div(java_total_calls).mul(100)
-df_java['formatted'] = df_java.apply(lambda x: "{:,.0f} ({:,.2f}%)".format(x.project, x.percent), axis=1)
-print(df_java.formatted)
+
 
 df_out = pd.DataFrame()
-df_out['Kotlin'] = df_kotlin.formatted
-df_out['Java'] = df_java.formatted
+df_out[('Java','Count')] = df_java.project
+df_out[('Java','Percent')] = df_java.percent.apply(lambda x: "({:,.2f}%)".format(x))
+df_out[('Kotlin','Count')] = df_kotlin.project
+df_out[('Kotlin','Percent')] = df_kotlin.percent.apply(lambda x: "({:,.2f}%)".format(x))
 
-styler = highlight_rows(highlight_cols(get_styler(df_out)))
-save_table(styler, 'rq-rhs-methodcalls.tex', column_format='lrr')
+styler = highlight_rows(highlight_cols(get_styler(df_out))).hide(axis='columns', level=1)
+save_table(styler, 'rq-rhs-methodcalls.tex', column_format='lrrrr')
