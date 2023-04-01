a2cbd0ccd3d73a89a3ace1281cbf63b342726860
Robert Dyer <rdyer@unl.edu>
2023-03-31 21:41:56 -0500

    remove unused file

--- bin/filter-jdk10.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# coding: utf-8
-
-import sys
-
-with open(sys.argv[1], 'r') as f:
-    for id in [l.split('=')[-1].strip() for l in f.readlines()]:
-        print(f'add(jdk10projects, "{id}");')
