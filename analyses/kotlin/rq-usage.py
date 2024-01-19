daacafc8bbb483595b2b8e3031a95f294b92a69e
Samuel W. Flint <swflint@flintfam.org>
2024-01-19 11:18:42 -0600

    Start building a model instead

--- analyses/kotlin/rq-usage.py
+++ analyses/kotlin/rq-usage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# %% build the dataframe
+# %% Import tools
 from pathlib import Path
 import sys
 sys.path.append(str(Path(__file__).resolve().parent.parent))
@@ -11,9 +11,29 @@ from common.local import *
 from common.tables import *
 import matplotlib.ticker as mtick
 
+import statsmodels.api as sm
+import statsmodels.graphics as smg
+import statsmodels.formula.api as smf
+import statsmodels.stats as sms
+import scipy.stats as stats
+
+# %% Load Data
+
 summarized = load_pre_summarized('kotlin',
                                  ['project', 'location', 'isinferred'])
 summarized = summarized[summarized.total > 0]
+
+# %% Build model
+
+summarized_temp = summarized.assign(location = lambda df: df.location.str.replace('\n', ' '))
+
+model = smf.ols('center(percent) ~ C(isinferred)*C(location) - 1', data=summarized_temp).fit()
+
+# %% Show the model and print
+
+print(model.summary())
+
+sms.anova.anova_lm(model, typ=3).convert_dtypes()
 
 # %% generate the plot
 fig, ax = setup_plots()
